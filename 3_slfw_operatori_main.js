6f87aa95ae8e7f3b403c1b9718dc0a14d5d41cc1
Dario Mauro <sitiwebdm78@gmail.com>
2025-04-02 10:37:17 +0200

    Ho corretto l'esercizio trasformando il dato stringa di prompt in tipo dato numerico

--- 3_slfw_operatori_main.js
+++ 3_slfw_operatori_main.js
@@ -2,9 +2,9 @@
 
 // DICHIARO E INIZIALIZZO LE VARIABILI ACQUISENDO IL DATO NUMERICO DALL'UTENTE.
 
-    let num1 = prompt('Scegli il Primo numero.'); // Acquisisco dall'utente il primo dato numerico.
-    let num2 = prompt('Scegli il Secondo numero.'); // Acquisisco dall'utente il secondo dato numerico.
-    let esponente = prompt('Scegli un numero diesponente di potenza'); //Acquisisco Il dato numerico per eseguire l'elevamento a potenza.
+    let num1 = parseInt(prompt('Scegli il Primo numero.')); // Acquisisco dall'utente il primo Input, trasformando il dato stringa ricevuto dal prompt in numerico, con parseint.
+    let num2 = parseInt(prompt('Scegli il Secondo numero.'));// Acquisisco dall'utente il Secondo Input, trasformando il dato stringa ricevuto dal prompt in numerico, con parseint.
+    let esponente = parseInt(prompt('Scegli il Terzo numero da elevare a potenza.')); // Acquisisco dall'utente il terzo Input, trasformando il dato stringa ricevuto dal prompt in numerico, con parseint.
 
     // ESEGUO LE OPERAZIONI RICHIESTE DALLA TRACCIA DELL'ESERCIZIO I CUI RISULTATI VENGONO SALVATI NELLERELATIVE VARIABILI.
 
@@ -15,8 +15,21 @@
     let potenza1 = num1 ** esponente;
     let potenza2 = num2 ** esponente;
 
-    console.log(`In base ai numeri da te scelti, si sono ricavati dalle relative operazioni i seguenti risultati : Somma = ${somma}, Sottrazione = ${sottrazione}, Moltiplicazione = ${moltiplicazione}, Divisione = ${divisione}, Elevamento a Potenza del primo numero = ${potenza1}, Elevamento a Potenza del secondo numero = ${potenza2} `);
-    
+    console.log('In base ai numeri da te scelti, si sono ricavati dalle relative operazioni i seguenti risultati:');
+    console.log(`Somma = ${somma}
+Sottrazione = ${sottrazione} 
+Moltiplicazione = ${moltiplicazione} 
+Divisione = ${divisione} 
+Elevamento a Potenza del primo numero = ${potenza1} 
+Elevamento a Potenza del secondo numero = ${potenza2}`);
+
+    console.log(`Somma = ${typeof somma}
+Sottrazione = ${typeof sottrazione} 
+Moltiplicazione = ${typeof moltiplicazione} 
+Divisione = ${typeof divisione} 
+Elevamento a Potenza del primo numero = ${typeof potenza1} 
+Elevamento a Potenza del secondo numero = ${typeof potenza2}`);
+
 
 
 
